c75aec4b36bd14da61f253be9b9491a752ce4ced
mjz19910 <matthias291999@gmail.com>
2021-09-10 11:20:13 -0700

    u

--- src.rs
+++ src.rs
@@ -1,18 +1,19 @@
 macro_rules! raw_size {
 	() => {
-		0x8
+		0xc
 	};
 }
 type UnsafeSliceU8=[u8;raw_size!()*std::mem::size_of::<usize>()];
 
+type UnsafeSliceUsize=[usize;raw_size!()];
+
 fn main() -> () {
 	let raw_slice=unsafe { std::mem::transmute::<_,&UnsafeSliceU8>(main as fn()) };
-	let raw_slice_struct=unsafe { std::mem::transmute::<_,&Box<A>>(main as fn()) };
+	let _mark : u64=0xdeadbeef;
+	let mut raw_slice_struct=unsafe { std::mem::transmute::<_,&A>(&(main as fn())) };
 	println!("{}",format!("{:x?}",raw_slice).replace(", ","").replace('[',"").replace(']',""));
-	println!("{:x?}",&raw_slice_struct._v1);
+	println!("{}",format!("{:#014X?}",raw_slice_struct.0).replace(",\n    ",",\n").replace("[\n    ","").replace("\n]",""));
 	()
 }
 
-struct A {
-	_v1:UnsafeSliceU8
-}
+struct A (UnsafeSliceUsize);
