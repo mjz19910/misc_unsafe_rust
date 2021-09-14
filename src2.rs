808f24cf53b74891cc37ef53cff0d1f892d373a7
mjz19910 <matthias291999@gmail.com>
2021-09-14 16:14:22 -0700

    u

--- src2.rs
+++ src2.rs
@@ -9,21 +9,25 @@ extern "C" fn eh_personality() {}
 fn panic(_: &PanicInfo) -> ! {
     extern "C" {
         #[cfg_attr(
-            target_family = "unix", link_name = "\n\n\x1b[s\x1b[1000D\x1b[0;31m\x1b[1merror\x1b[0m\x1b[1m: the static assertion that no panics are present has failed\x1b[0m\x1b[u\n\n"
+            target_family = "unix",
+            link_name = "\n\n\x1b[s\x1b[1000D\x1b[0;31m\x1b[1merror\x1b[0m\x1b[1m: the static assertion that no panics are present has failed\x1b[0m\x1b[u\n\n"
         )]
-        #[cfg_attr(not(target_family = "unix"), link_name = "\n\nerror: the static assertion that no panics are present has failed\n\n")]
+        #[cfg_attr(
+            not(target_family = "unix"),
+            link_name = "\n\nerror: the static assertion that no panics are present has failed\n\n"
+        )]
         fn never_panic() -> !;
     }
-    
+
     unsafe { never_panic() }
 }
 #[no_mangle]
-pub extern fn __libc_csu_fini(){}
+pub extern "C" fn __libc_csu_fini() {}
 #[no_mangle]
-pub extern fn __libc_csu_init(){}
+pub extern "C" fn __libc_csu_init() {}
 #[no_mangle]
-pub extern "C" fn __libc_start_main(){
-    unsafe{
+pub extern "C" fn __libc_start_main() {
+    unsafe {
         asm!("pop rdi");
         asm!("mov rsi,rsp");
         asm!("push rdi");
@@ -35,8 +39,8 @@ pub extern "C" fn __libc_start_main(){
 }
 #[no_mangle]
 pub extern "C" fn main(_argc: isize, _argv: *const *const u8) -> isize {
-let x: Option<u8> = None;
-return 0;
-let _ = unsafe{x.unwrap_unchecked()};
-0
+    let x: Option<u8> = None;
+    return 0;
+    let _ = unsafe { x.unwrap_unchecked() };
+    0
 }
