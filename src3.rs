dd267dc6c0c5986a93d357e46b2046704e3c66ed
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-02 17:42:21 -0700

    u

--- src3.rs
+++ src3.rs
@@ -1,15 +1,15 @@
 use std::slice;
 
 fn main() {
-    let fn_ref = &&(main as fn());
+    let fn_ref = &[main];
     let ptr = unsafe {
-        let ptr2 = fn_ref as *const _ as *const *const usize;
+        let ptr2 = (*fn_ref.as_ptr()) as *const usize;
         (
-            **ptr2,
-            slice::from_raw_parts(**ptr2 as *const u8, 7),
+            ptr2,
+            slice::from_raw_parts(ptr2 as *const u8, 12),
         )
     };
     let print_ptr = ptr.0 as usize;
-    println!("0x{:x}", print_ptr);
+    println!("{:#x}", print_ptr);
     println!("{:x?}", ptr.1);
 }
