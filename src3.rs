049baa3b0c1c798cf1b959760483d03ccc6210cf
Matthias Zimmerman <matthias291999@gmail.com>
2022-10-02 19:37:14 -0700

    u

--- src3.rs
+++ src3.rs
@@ -6,22 +6,26 @@ fn main() {
     let fn_ptr = fn_ref.as_ptr();
     let ptr = unsafe {
         let ptr2 = (*fn_ptr) as *const usize;
-        (ptr2 as usize, slice::from_raw_parts(ptr2 as *const u8, 12))
+        (
+            ptr2 as usize,
+            (fn_ptr as *const usize) as usize,
+            slice::from_raw_parts(ptr2 as *const u8, 12)
+        )
     };
     assert_eq!(mem::size_of_val(fn_ref), 0);
-    println!("{:#x}", ptr.0);
-    println!("{:#x}", (fn_ptr as *const usize) as usize);
-    println!("{:x?}", ptr.1);
+    println!("{:#x} -> {:#x}", ptr.0, ptr.1);
+    println!("{:#x} -> {:x?}", ptr.1, ptr.2);
     // cspell:disable-next-line
-    let x = 0x784ffcc_u64;
-    let fn_2 = ||x;
-    let fn2_ptr = &fn_2 as *const _;
+    let x = 0x4151u64;
+    let fn_2 = &(||x) as &dyn Fn() -> _;
+    let fn2_ptr = fn_2 as *const _;
     println!("{:x?}", unsafe {
         let a = fn2_ptr as *const usize;
         (
             fn2_ptr,
             mem::size_of_val(&fn_2),
             slice::from_raw_parts(a, mem::size_of_val(&fn_2) / 8),
+            slice::from_raw_parts((*a) as *const usize, 1),
         )
     });
 }
